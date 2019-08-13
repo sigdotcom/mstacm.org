7b455707bb38ddf0fe8157d63fb53ecc72a3c4e2
Kevin Schoonover <schoonoverkevinm@gmail.com>
2019-08-12 23:53:03 -0700

    Strip down HomePage to hero

--- apps/web/src/components/pages/HomePage/index.tsx
+++ apps/web/src/components/pages/HomePage/index.tsx
@@ -1,13 +1,13 @@
 import * as React from "react";
 import styled from "styled-components";
 
-import About from "./sections/About";
-import Events from "./sections/Events";
-import { Footer } from "./sections/Footer";
+// import About from "./sections/About";
+// import Events from "./sections/Events";
+// import { Footer } from "./sections/Footer";
 import Hero from "./sections/Hero";
 import { Nav } from "./sections/Nav";
-import { SIGs } from "./sections/SIGs";
-import { Sponsors } from "./sections/Sponsors";
+// import { SIGs } from "./sections/SIGs";
+// import { Sponsors } from "./sections/Sponsors";
 
 const NavHeroWrapper = styled.div`
   height: 100vh;
@@ -30,11 +30,6 @@ const HomePage: React.FC<{}> = () => {
         <Nav />
         <Hero />
       </NavHeroWrapper>
-      <About />
-      <SIGs />
-      <Events />
-      <Sponsors />
-      <Footer />
     </div>
   );
 };
--- apps/web/src/components/pages/HomePage/sections/Nav.tsx
+++ apps/web/src/components/pages/HomePage/sections/Nav.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-scroll";
+// import { Link } from "react-scroll";
 import styled from "styled-components";
 
 import { PageConstraint } from "../../../common/PageConstraint";
@@ -21,46 +21,46 @@ const Logo = styled.img`
   height: 85px;
 `;
 
-const MenuItems = styled.div`
-  display: none;
-
-  @media screen and (min-width: 700px) {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 340px;
-    margin-left: 30px;
-  }
-`;
-
-const MenuItem = styled.div`
-  color: #376b83;
-  font-weight: 600;
-  font-size: 18px;
-  padding: 5px 10px;
-`;
-
-const SignIn = styled.button`
-  transition: 0.2s ease-in-out;
-  font-weight: bold;
-  border-radius: 30px;
-  padding: 10px 40px
-  font-size: 17px;
-  color: #2d9cdb;
-  border: 3px solid #2d9cdb;
-  cursor: pointer;
-  &:hover {
-    background: #2d9cdb;
-    color: #fff;
-  }
-`;
+// const MenuItems = styled.div`
+//   display: none;
+//
+//   @media screen and (min-width: 700px) {
+//     display: flex;
+//     justify-content: space-between;
+//     align-items: center;
+//     width: 340px;
+//     margin-left: 30px;
+//   }
+// `;
+//
+// const MenuItem = styled.div`
+//   color: #376b83;
+//   font-weight: 600;
+//   font-size: 18px;
+//   padding: 5px 10px;
+// `;
+//
+// const SignIn = styled.button`
+//   transition: 0.2s ease-in-out;
+//   font-weight: bold;
+//   border-radius: 30px;
+//   padding: 10px 40px
+//   font-size: 17px;
+//   color: #2d9cdb;
+//   border: 3px solid #2d9cdb;
+//   cursor: pointer;
+//   &:hover {
+//     background: #2d9cdb;
+//     color: #fff;
+//   }
+// `;
 
 const NavRow: any = styled.nav`
   display: flex;
 `;
 
 const Nav: React.FC<{}> = () => {
-  const handleSignIn = () => {};
+  // const handleSignIn = () => {};
 
   return (
     <BG>
@@ -68,22 +68,7 @@ const Nav: React.FC<{}> = () => {
         <Wrapper>
           <NavRow>
             <Logo src={require("../../../../static/img/acm-logo.png")} />
-            <MenuItems>
-              <Link to="about" smooth={true}>
-                <MenuItem>About</MenuItem>
-              </Link>
-              <Link to="groups" smooth={true}>
-                <MenuItem>Groups</MenuItem>
-              </Link>
-              <Link to="events" smooth={true}>
-                <MenuItem>Events</MenuItem>
-              </Link>
-              <Link to="sponsors" smooth={true}>
-                <MenuItem>Sponsors</MenuItem>
-              </Link>
-            </MenuItems>
           </NavRow>
-          <SignIn onClick={handleSignIn}>SIGN IN</SignIn>
         </Wrapper>
       </PageConstraint>
     </BG>
