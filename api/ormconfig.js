64e912712d85a5a7338178735756389cd4ca7c21
Kevin Schoonover <schoonoverkevinm@gmail.com>
2019-08-28 15:50:03 -0500

    Fix the ability to generate migrations

--- api/ormconfig.js
+++ api/ormconfig.js
@@ -1,5 +1,5 @@
-const ROOT_DIR = process.env.NODE_ENV != "production" ? "src/" : "build/";
-const EXT = process.env.NODE_ENV != "production" ? ".ts" : ".js";
+const IS_PROD = process.env.NODE_ENV === "production";
+const ROOT_DIR = IS_PROD ? "build" : "src";
 
 module.exports = {
   type: "postgres",
@@ -8,17 +8,17 @@ module.exports = {
   password: process.env.DB_PASSWORD || "phoenix",
   database: process.env.DB_TABLE || "phoenix",
   port: process.env.DB_PORT || 5432,
-  synchronize: true,
+  synchronize: IS_PROD ? false : true,
   logging: "all",
   logger: "advanced-console",
-  dropSchema: true,
+  dropSchema: IS_PROD ? false : true,
   cache: true,
-  entities: [ROOT_DIR + "/**/entity" + EXT],
-  migrations: [ROOT_DIR + "migrations/**/*" + EXT],
-  subscribers: [ROOT_DIR + "subscribers/**/*" + EXT],
+  entities: [`${ROOT_DIR}/resources/**/index{.js,.ts}`],
+  migrations: [`${ROOT_DIR}/migrations/**/*{.js,.ts}`],
+  subscribers: [`${ROOT_DIR}/subscribers/**/*{.js.ts}`],
   cli: {
-    entitiesDir: ROOT_DIR + "entities",
-    migrationsDir: ROOT_DIR + "migrations",
-    subscribersDir: ROOT_DIR + "subscribers"
+    entitiesDir: `${ROOT_DIR}/entities`,
+    migrationsDir: `${ROOT_DIR}/migrations`,
+    subscribersDir: `${ROOT_DIR}/subscribers`
   }
 };
