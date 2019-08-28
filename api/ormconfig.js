d6a552c34dd727f4c593b7a339aa8c3b0ccb441a
Kevin Schoonover <schoonoverkevinm@gmail.com>
2019-08-28 16:17:32 -0500

    Fix entity detection in production

--- api/ormconfig.js
+++ api/ormconfig.js
@@ -1,5 +1,5 @@
 const IS_PROD = process.env.NODE_ENV === "production";
-const ROOT_DIR = IS_PROD ? "build" : "src";
+const ROOT_DIR = IS_PROD ? "build/src" : "src";
 
 module.exports = {
   type: "postgres",
@@ -8,9 +8,13 @@ module.exports = {
   password: process.env.DB_PASSWORD || "phoenix",
   database: process.env.DB_TABLE || "phoenix",
   port: process.env.DB_PORT || 5432,
+  // Should we automatically synchronize our database?
   synchronize: IS_PROD ? false : true,
   logging: "all",
+  // Run migrations automatically,
+  migrationsRun: IS_PROD ? true : false,
   logger: "advanced-console",
+  // Should we automatically drop the entire database on start?
   dropSchema: IS_PROD ? false : true,
   cache: true,
   entities: [`${ROOT_DIR}/resources/**/index{.js,.ts}`],
