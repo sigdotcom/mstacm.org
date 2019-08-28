cc071f5bd09d8ca9de3ce629c01b911575c003f8
Kevin Schoonover <schoonoverkevinm@gmail.com>
2019-08-28 15:50:03 -0500

    Fix the ability to generate migrations

--- api/src/lib/interfaces.ts
+++ api/src/lib/interfaces.ts
@@ -1,8 +1,3 @@
 import { ParameterizedContext } from "koa";
-import { User } from "../User";
 
 export interface IContext extends ParameterizedContext {}
-
-interface IOwnership {
-  isOwner(user: User): boolean;
-}
