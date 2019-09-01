9b88b3a7905aa3a1b91270bcde8bbf1e6f06c86a
Kevin Schoonover <schoonoverkevinm@gmail.com>
2019-09-01 10:13:38 -0500

    Fix `App.test.tsx` to include Apollo + Auth0 context

--- apps/web/src/App.test.tsx
+++ apps/web/src/App.test.tsx
@@ -1,9 +1,28 @@
+import { ApolloProvider } from "@apollo/react-hooks";
 import React from "react";
 import ReactDOM from "react-dom";
+
 import { App } from "./App";
+import { config } from "./config";
+import { client } from "./utils/apollo";
+import { Auth0Provider, onRedirectCallback } from "./utils/react-auth0-wrapper";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+
+  ReactDOM.render(
+    <Auth0Provider
+      domain={config.AUTH0_DOMAIN}
+      client_id={config.AUTH0_CLIENT_ID}
+      redirect_uri={window.location.origin}
+      audience={config.AUTH0_AUDIENCE}
+      onRedirectCallback={onRedirectCallback}
+    >
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </Auth0Provider>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
