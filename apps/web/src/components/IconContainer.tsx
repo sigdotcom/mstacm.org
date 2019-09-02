5a727838ecae529f7b9877fa3c6562a87f1c7579
Kevin Schoonover <schoonoverkevinm@gmail.com>
2019-09-02 11:04:54 -0500

    Add confirmation on successful membership payment

---
+++ apps/web/src/components/IconContainer.tsx
@@ -0,0 +1,10 @@
+import styled, { AnyStyledComponent } from "styled-components";
+
+export const IconContainer: AnyStyledComponent = styled.span`
+  .eva-hover {
+    display: flex
+    align-content: center;
+    justify-content: center;
+    align-items: center;
+  }
+`;
