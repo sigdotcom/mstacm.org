7cbcc8b45d16459039b25dea31912b29ab890f22
Kevin Schoonover <schoonoverkevinm@gmail.com>
2019-09-02 11:08:01 -0500

    Remove 'hello world' route in api

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -6,14 +6,6 @@ import * as Router from "koa-router";
 const router = new Router();
 
 /**
- * Base route, return a 401
- */
-router.get("", async (ctx: Koa.ParameterizedContext) => {
-  ctx.body = "Hello, World!";
-  ctx.status = 200;
-});
-
-/**
  * Basic healthcheck
  */
 router.get("healthcheck/", async (ctx: Koa.ParameterizedContext) => {
