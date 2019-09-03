a2bd4802abd718a9562c0cf50dc330518ef8834b
Kevin Schoonover <schoonoverkevinm@gmail.com>
2019-09-02 23:30:43 -0500

    Add generated `graphql.tsx` to repo for CircleCi

---
+++ apps/web/src/generated/graphql.tsx
@@ -0,0 +1,320 @@
+import gql from 'graphql-tag';
+import * as ApolloReactCommon from '@apollo/react-common';
+import * as ApolloReactHooks from '@apollo/react-hooks';
+export type Maybe<T> = T | null;
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string,
+  String: string,
+  Boolean: boolean,
+  Int: number,
+  Float: number,
+  /** The javascript `Date` as string. Type represents date and time as the ISO Date string. */
+  DateTime: any,
+  /** The `Upload` scalar type represents a file upload. */
+  Upload: any,
+};
+
+
+/** The potential errors codes that will be sent to a user. */
+export enum ErrorCodes {
+  InternalServerError = 'INTERNAL_SERVER_ERROR',
+  ResourceNotFound = 'RESOURCE_NOT_FOUND',
+  Unauthenticated = 'UNAUTHENTICATED',
+  BadUserInput = 'BAD_USER_INPUT'
+}
+
+export type Event = {
+   __typename?: 'Event',
+  id: Scalars['ID'],
+  dateCreated: Scalars['DateTime'],
+  dateHosted: Scalars['DateTime'],
+  dateExpire: Scalars['DateTime'],
+  creator: User,
+  eventTitle: Scalars['String'],
+  description: Scalars['String'],
+  location: Scalars['String'],
+  flierLink?: Maybe<Scalars['String']>,
+  eventLink?: Maybe<Scalars['String']>,
+};
+
+export type EventCreateInput = {
+  eventTitle: Scalars['String'],
+  dateHosted: Scalars['DateTime'],
+  dateExpire: Scalars['DateTime'],
+  description: Scalars['String'],
+  location: Scalars['String'],
+  flierLink?: Maybe<Scalars['String']>,
+  eventLink?: Maybe<Scalars['String']>,
+};
+
+export type EventDeletePayload = {
+   __typename?: 'EventDeletePayload',
+  id?: Maybe<Scalars['Float']>,
+};
+
+export type EventUpdateInput = {
+  eventTitle?: Maybe<Scalars['String']>,
+  dateHosted?: Maybe<Scalars['DateTime']>,
+  dateExpire?: Maybe<Scalars['DateTime']>,
+  description?: Maybe<Scalars['String']>,
+  location?: Maybe<Scalars['String']>,
+  flierLink?: Maybe<Scalars['String']>,
+  eventLink?: Maybe<Scalars['String']>,
+};
+
+export type Extension = {
+   __typename?: 'Extension',
+  code: ErrorCodes,
+};
+
+/** Different types of ACM memberships one can have. */
+export enum MembershipTypes {
+  Yearly = 'YEARLY',
+  Semesterly = 'SEMESTERLY'
+}
+
+export type Mutation = {
+   __typename?: 'Mutation',
+  createUser: User,
+  updateUser: User,
+  deleteUser: UserDeletePayload,
+  deleteEvent: EventDeletePayload,
+  updateEvent: Event,
+  createEvent: Event,
+  createPermission: Permission,
+  deleteResume: User,
+  uploadResume: Resume,
+  startMembershipTransaction: TransactionPayload,
+  startTransaction: Transaction,
+};
+
+
+export type MutationCreateUserArgs = {
+  data: UserCreateInput
+};
+
+
+export type MutationUpdateUserArgs = {
+  data: UserUpdateInput,
+  id: Scalars['String']
+};
+
+
+export type MutationDeleteUserArgs = {
+  id: Scalars['String']
+};
+
+
+export type MutationDeleteEventArgs = {
+  id: Scalars['Float']
+};
+
+
+export type MutationUpdateEventArgs = {
+  data: EventUpdateInput
+};
+
+
+export type MutationCreateEventArgs = {
+  data: EventCreateInput
+};
+
+
+export type MutationCreatePermissionArgs = {
+  data: PermissionCreateInput
+};
+
+
+export type MutationUploadResumeArgs = {
+  resume: Scalars['Upload']
+};
+
+
+export type MutationStartMembershipTransactionArgs = {
+  membershipType: MembershipTypes
+};
+
+
+export type MutationStartTransactionArgs = {
+  purchases: Array<PurchaseInput>
+};
+
+export type Permission = {
+   __typename?: 'Permission',
+  name: Scalars['ID'],
+  users: Array<User>,
+};
+
+export type PermissionCreateInput = {
+  name: Scalars['String'],
+};
+
+export type PermissionDeletePayload = {
+   __typename?: 'PermissionDeletePayload',
+  name?: Maybe<Scalars['String']>,
+};
+
+export type PermissionUpdateInput = {
+  name?: Maybe<Scalars['String']>,
+};
+
+export type Product = {
+   __typename?: 'Product',
+  tag: Scalars['ID'],
+  displayName: Scalars['String'],
+  description: Scalars['String'],
+  price: Scalars['Float'],
+};
+
+export type Purchase = {
+   __typename?: 'Purchase',
+  id: Scalars['ID'],
+  quantity: Scalars['Float'],
+  product: Product,
+  transaction: Array<Transaction>,
+};
+
+export type PurchaseInput = {
+  quantity: Scalars['Float'],
+  tag: Scalars['String'],
+};
+
+export type Query = {
+   __typename?: 'Query',
+  user: User,
+  users: Array<User>,
+  events: Array<Event>,
+  event: Event,
+  permissions: Array<Permission>,
+  products: Array<Product>,
+  transactions: Array<Transaction>,
+  me?: Maybe<User>,
+};
+
+
+export type QueryUserArgs = {
+  id: Scalars['String']
+};
+
+
+export type QueryEventArgs = {
+  id: Scalars['Float']
+};
+
+export type Resume = {
+   __typename?: 'Resume',
+  id: Scalars['ID'],
+  url: Scalars['String'],
+  added: Scalars['DateTime'],
+  user?: Maybe<User>,
+};
+
+export type Transaction = {
+   __typename?: 'Transaction',
+  id: Scalars['ID'],
+  intent: Scalars['String'],
+  charged: Scalars['Float'],
+  status: Scalars['String'],
+  user: User,
+  purchases: Array<Purchase>,
+};
+
+export type TransactionPayload = {
+   __typename?: 'TransactionPayload',
+  id: Scalars['String'],
+  charged?: Maybe<Scalars['Float']>,
+  clientSecret: Scalars['String'],
+};
+
+
+export type User = {
+   __typename?: 'User',
+  id: Scalars['ID'],
+  firstName: Scalars['String'],
+  lastName: Scalars['String'],
+  email: Scalars['String'],
+  emailVerified: Scalars['Boolean'],
+  isSuperAdmin?: Maybe<Scalars['Boolean']>,
+  dateJoined: Scalars['DateTime'],
+  membershipExpiration?: Maybe<Scalars['DateTime']>,
+  isActive?: Maybe<Scalars['Boolean']>,
+  resume?: Maybe<Resume>,
+  permissions: Array<Permission>,
+};
+
+export type UserCreateInput = {
+  firstName: Scalars['String'],
+  lastName: Scalars['String'],
+  email: Scalars['String'],
+  sub: Scalars['String'],
+};
+
+export type UserDeletePayload = {
+   __typename?: 'UserDeletePayload',
+  firstName?: Maybe<Scalars['String']>,
+};
+
+export type UserUpdateInput = {
+  firstName?: Maybe<Scalars['String']>,
+  lastName?: Maybe<Scalars['String']>,
+  email?: Maybe<Scalars['String']>,
+};
+export type GetMembershipMutationVariables = {
+  membership: MembershipTypes
+};
+
+
+export type GetMembershipMutation = (
+  { __typename?: 'Mutation' }
+  & { startMembershipTransaction: (
+    { __typename?: 'TransactionPayload' }
+    & Pick<TransactionPayload, 'id' | 'charged' | 'clientSecret'>
+  ) }
+);
+
+export type MeExpirationQueryVariables = {};
+
+
+export type MeExpirationQuery = (
+  { __typename?: 'Query' }
+  & { me: Maybe<(
+    { __typename?: 'User' }
+    & Pick<User, 'membershipExpiration'>
+  )> }
+);
+
+export const GetMembershipDocument = gql`
+    mutation GetMembership($membership: MembershipTypes!) {
+  startMembershipTransaction(membershipType: $membership) {
+    id
+    charged
+    clientSecret
+  }
+}
+    `;
+export type GetMembershipMutationFn = ApolloReactCommon.MutationFunction<GetMembershipMutation, GetMembershipMutationVariables>;
+
+    export function useGetMembershipMutation(baseOptions?: ApolloReactHooks.MutationHookOptions<GetMembershipMutation, GetMembershipMutationVariables>) {
+      return ApolloReactHooks.useMutation<GetMembershipMutation, GetMembershipMutationVariables>(GetMembershipDocument, baseOptions);
+    }
+export type GetMembershipMutationHookResult = ReturnType<typeof useGetMembershipMutation>;
+export type GetMembershipMutationResult = ApolloReactCommon.MutationResult<GetMembershipMutation>;
+export type GetMembershipMutationOptions = ApolloReactCommon.BaseMutationOptions<GetMembershipMutation, GetMembershipMutationVariables>;
+export const MeExpirationDocument = gql`
+    query MeExpiration {
+  me {
+    membershipExpiration
+  }
+}
+    `;
+
+    export function useMeExpirationQuery(baseOptions?: ApolloReactHooks.QueryHookOptions<MeExpirationQuery, MeExpirationQueryVariables>) {
+      return ApolloReactHooks.useQuery<MeExpirationQuery, MeExpirationQueryVariables>(MeExpirationDocument, baseOptions);
+    }
+      export function useMeExpirationLazyQuery(baseOptions?: ApolloReactHooks.LazyQueryHookOptions<MeExpirationQuery, MeExpirationQueryVariables>) {
+        return ApolloReactHooks.useLazyQuery<MeExpirationQuery, MeExpirationQueryVariables>(MeExpirationDocument, baseOptions);
+      }
+      
+export type MeExpirationQueryHookResult = ReturnType<typeof useMeExpirationQuery>;
+export type MeExpirationQueryResult = ApolloReactCommon.QueryResult<MeExpirationQuery, MeExpirationQueryVariables>;
