0a76a7eb36510dfbe1c5119ba92686e62f853b5b
Clay McGinnis <mcginnis@claymav.com>
2019-09-11 17:43:20 -0500

    Use new test tenant for Auth0 local

--- apps/admin-web/src/config.ts
+++ apps/admin-web/src/config.ts
@@ -17,6 +17,6 @@ export const config: IConfig = {
   REDIRECT_PAGE_URI: IS_PROD ? "https://mstacm.org" : "http://localhost:3000",
   AUTH0_AUDIENCE: process.env.REACT_APP_AUTH0_AUDIENCE || "graphql.mstacm.org",
   AUTH0_CLIENT_ID:
-    process.env.REACT_APP_AUTH0_CLIENT_ID || "U5F87knjS1tfcO7iDqQgd8Q3EeH6BnZt",
-  AUTH0_DOMAIN: process.env.REACT_APP_AUTH0_DOMAIN || "mstacm.auth0.com"
+    process.env.REACT_APP_AUTH0_CLIENT_ID || "na6x6uijGdUE2zpiu9MqmSRQwGUdhNQl",
+  AUTH0_DOMAIN: process.env.REACT_APP_AUTH0_DOMAIN || "mstacm-test.auth0.com"
 };
