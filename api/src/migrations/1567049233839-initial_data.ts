26623d21268cbe43203280793ab8f6a8f003b283
Kevin Schoonover <schoonoverkevinm@gmail.com>
2019-09-11 16:58:43 -0500

    Fix initial data migration to support Entity changes

--- api/src/migrations/1567049233839-initial_data.ts
+++ api/src/migrations/1567049233839-initial_data.ts
@@ -10,68 +10,25 @@ const DEFAULT_SUB = "google-oauth2|115625753701019295484";
  */
 export class initialData1567049233839 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<any> {
-    const queryBuilder = await queryRunner.manager.createQueryBuilder();
-    await queryBuilder
-      .insert()
-      .into("user")
-      .values([
-        {
-          sub: DEFAULT_SUB,
-          firstName: "MST",
-          lastName: "ACM",
-          email: "acm@mst.edu",
-          emailVerified: true,
-          isSuperAdmin: true
-        }
-      ])
-      .execute();
+    await queryRunner.query(
+      `INSERT INTO "user" ("sub", "firstName", "lastName", "email", "emailVerified", "isSuperAdmin")
+                   VALUES ('${DEFAULT_SUB}', 'MST', 'ACM', 'acm@mst.edu', true, true)`
+    );
 
-    await queryBuilder
-      .insert()
-      .into("product")
-      .values([
-        {
-          tag: SEMESTERLY_MEMBERSHIP.tag,
-          displayName: "ACM Semesterly Membership",
-          description:
-            "ACM Membership purchase for a single semester (6 months).",
-          price: 11
-        }
-      ])
-      .execute();
-    await queryBuilder
-      .insert()
-      .into("product")
-      .values([
-        {
-          tag: YEARLY_MEMBERSHIP.tag,
-          displayName: "ACM Yearly Membership",
-          description: "ACM Membership purchase for a single year (12 months).",
-          price: 20
-        }
-      ])
-      .execute();
+    await queryRunner.query(
+      `INSERT INTO "product" ("tag", "displayName", "description", "price")
+                      VALUES ('${SEMESTERLY_MEMBERSHIP.tag}', 'ACM Semesterly Membership', 'ACM Membership purchase for a single semester (6 months).', 11),
+                             ('${YEARLY_MEMBERSHIP.tag}', 'ACM Yearly Membership', 'ACM Membership purchase for a single year (12 months).', 20)`
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<any> {
-    const queryBuilder = await queryRunner.manager.createQueryBuilder();
-
-    await queryBuilder
-      .delete()
-      .from("user")
-      .where("sub = :sub", { sub: DEFAULT_SUB })
-      .execute();
-
-    await queryBuilder
-      .delete()
-      .from("product")
-      .where("tag = :tag", { tag: SEMESTERLY_MEMBERSHIP.tag })
-      .execute();
-
-    await queryBuilder
-      .delete()
-      .from("product")
-      .where("tag = :tag", { tag: YEARLY_MEMBERSHIP.tag })
-      .execute();
+    await queryRunner.query(`DELETE FROM "user" where sub='${DEFAULT_SUB}'`);
+    await queryRunner.query(
+      `DELETE FROM "product" where tag='${SEMESTERLY_MEMBERSHIP.tag}'`
+    );
+    await queryRunner.query(
+      `DELETE FROM "product" where tag='${YEARLY_MEMBERSHIP.tag}'`
+    );
   }
 }
