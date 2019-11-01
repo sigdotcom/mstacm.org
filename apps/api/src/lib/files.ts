62cddf2d71a3c1b0576e6170429ac026eaaf8dc2
Kevin Schoonover <schoonoverkevinm@gmail.com>
2019-10-31 19:48:02 -0500

    Update types for lib/files.ts and Resume resolver

--- apps/api/src/lib/files.ts
+++ apps/api/src/lib/files.ts
@@ -2,14 +2,14 @@ import AWS from "aws-sdk";
 import { Readable } from "stream";
 import { config } from "../config";
 
-const s3 = new AWS.S3({
+const s3: AWS.S3 = new AWS.S3({
   endpoint: config.DO_SPACES_ENDPOINT,
   region: config.DO_SPACES_REGION,
   accessKeyId: config.DO_SPACES_ACCESS_KEY_ID,
   secretAccessKey: config.DO_SPACES_SECRET_KEY_ID
 });
 
-export const deleteFile = async (url: string) => {
+export const deleteFile: (url: string) => void = async (url: string) => {
   // The URL will be something like
   // https://mstacm-cdn-test.nyc3.digitaloceanspaces.com/test/UUID.pdf
   // so this will split it down the middle and just give the 'test/UUID.pdf'
@@ -23,12 +23,16 @@ export const deleteFile = async (url: string) => {
     .promise();
 };
 
-export const uploadFile = async (
+export const uploadFile: (
+  stream: Readable,
+  filename: string,
+  contentType?: string
+) => Promise<string> = async (
   stream: Readable,
   filename: string,
   contentType?: string
 ): Promise<string> => {
-  const response = await s3
+  const response: AWS.S3.ManagedUpload.SendData = await s3
     .upload({
       Bucket: config.DO_SPACES_CDN_BUCKET_NAME,
       Key: filename,
