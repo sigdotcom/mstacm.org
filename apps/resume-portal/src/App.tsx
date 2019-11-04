8aa9ff69c1181c058d0eb00319240f7ff9e306ee
Kevin Schoonover <schoonoverkevinm@gmail.com>
2019-11-03 20:53:49 -0600

    Add workflow for running frontend/backend tests

--- apps/resume-portal/src/App.tsx
+++ apps/resume-portal/src/App.tsx
@@ -151,4 +151,4 @@ const App: React.FC = () => {
   );
 };
 
-export default App;
+export { App };
--- apps/resume-portal/src/index.tsx
+++ apps/resume-portal/src/index.tsx
@@ -4,7 +4,7 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
+import { App } from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 import { config } from "./config";
