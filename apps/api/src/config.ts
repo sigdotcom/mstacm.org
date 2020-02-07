165ee5e215baf045feeaec97d1e3a25bad2ebae8
Kevin Schoonover <schoonoverkevinm@gmail.com>
2020-02-07 11:16:35 -0300

    Update api to throw error on missing config in production (#202)

--- apps/api/src/config.ts
+++ apps/api/src/config.ts
@@ -4,9 +4,6 @@ export interface IConfig {
   DO_SPACES_ENDPOINT: string;
   DO_SPACES_ACCESS_KEY_ID: string;
   DO_SPACES_SECRET_KEY_ID: string;
-  AZURE_CDN_URI: string;
-  AZURE_STORAGE_ACCOUNT: string;
-  AZURE_STORAGE_ACCOUNT_KEY: string;
   JWT_ALGORITHM: string;
   JWT_AUDIENCE: string;
   JWT_ISSUER: string;
@@ -22,30 +19,66 @@ export interface IConfig {
   port: number;
 }
 
-const { AUTH0_DOMAIN } = process.env;
 const DO_SPACES_REGION = process.env.DO_SPACES_REGION || "nyc3";
+
+const {
+  AUTH0_DOMAIN, // The domain used for authentication. Can be found at https://manage.auth0.com/dashboard/us/mstacm/tenant/custom_domains
+  DO_SPACES_CDN_BUCKET_NAME, // The bucket name used to store all CDN files on DigitalOcean.
+  DO_SPACES_ENDPOINT, // The endpoint used to serve the digitalocean spaces.
+  DO_SPACES_ACCESS_KEY_ID, // The access key for the digitalocean spaces.
+  DO_SPACES_SECRET_KEY_ID, // The secret key for the digitalocean spaces.
+  JWT_ALGORITHM,
+  JWT_AUDIENCE,
+  JWT_ISSUER,
+  JWT_JWKS_URI,
+  JWT_USERINFO_URI,
+  SECRET_APP_KEY, // Secret key used in KoaJS
+  STRIPE_PRIVATE_TOKEN, // The private token used to access Stripe.
+  STRIPE_PUBLIC_TOKEN, // The public token used to access Stripe.
+  STRIPE_WEBHOOK_SECRET // The signing secret used for the Stripe webhook. Can be found at https://dashboard.stripe.com/webhooks.
+} = process.env;
+
+const PRODUCTION_DEFINED_VARS: [string, string | undefined][] = [
+  ["AUTH0_DOMAIN", AUTH0_DOMAIN],
+  ["DO_SPACES_CDN_BUCKET_NAME", DO_SPACES_CDN_BUCKET_NAME],
+  ["DO_SPACES_ENDPOINT", DO_SPACES_ENDPOINT],
+  ["DO_SPACES_ACCESS_KEY_ID", DO_SPACES_ACCESS_KEY_ID],
+  ["DO_SPACES_SECRET_KEY_ID", DO_SPACES_SECRET_KEY_ID],
+  ["SECRET_APP_KEY", SECRET_APP_KEY],
+  ["STRIPE_PRIVATE_TOKEN", STRIPE_PRIVATE_TOKEN],
+  ["STRIPE_PUBLIC_TOKEN", STRIPE_PUBLIC_TOKEN],
+  ["STRIPE_WEBHOOK_SECRET", STRIPE_WEBHOOK_SECRET]
+];
+
+if (process.env.NODE_ENV === "production") {
+  const undefinedVariables = PRODUCTION_DEFINED_VARS.filter(item => {
+    return item[1] === undefined;
+  });
+
+  if (undefinedVariables.length > 0) {
+    const missingVariables = undefinedVariables.map(item => item[0]).join(", ");
+    const test = `The following variables are undefined in production ${missingVariables}`;
+    throw new Error(test);
+  }
+}
+
 export const config: IConfig = {
-  DO_SPACES_CDN_BUCKET_NAME:
-    process.env.DO_SPACES_CDN_BUCKET_NAME || "mstacm-cdn-test",
+  DO_SPACES_CDN_BUCKET_NAME: DO_SPACES_CDN_BUCKET_NAME || "mstacm-cdn-test",
   DO_SPACES_REGION,
   DO_SPACES_ENDPOINT:
-    process.env.DO_SPACES_ENDPOINT ||
-    `${DO_SPACES_REGION}.digitaloceanspaces.com`,
-  DO_SPACES_ACCESS_KEY_ID: process.env.DO_SPACES_ACCESS_KEY_ID || "changeme",
-  DO_SPACES_SECRET_KEY_ID: process.env.DO_SPACES_SECRET_KEY_ID || "changeme",
-  AZURE_CDN_URI: process.env.AZURE_CDN_URI || "https://cdn.mstacm.org",
-  AZURE_STORAGE_ACCOUNT: process.env.AZURE_STORAGE_ACCOUNT || "mstacm",
-  AZURE_STORAGE_ACCOUNT_KEY: process.env.AZURE_STORAGE_ACCOUNT_KEY || "DEV_KEY",
-  JWT_ALGORITHM: "RS256",
-  JWT_AUDIENCE: "graphql.mstacm.org",
-  JWT_ISSUER: `https://${AUTH0_DOMAIN}/`,
-  JWT_JWKS_URI: `https://${AUTH0_DOMAIN}/.well-known/jwks.json`,
-  JWT_USERINFO_URI: `https://${AUTH0_DOMAIN}/userinfo`,
+    DO_SPACES_ENDPOINT || `${DO_SPACES_REGION}.digitaloceanspaces.com`,
+  DO_SPACES_ACCESS_KEY_ID: DO_SPACES_ACCESS_KEY_ID || "changeme",
+  DO_SPACES_SECRET_KEY_ID: DO_SPACES_SECRET_KEY_ID || "changeme",
+  JWT_ALGORITHM: JWT_ALGORITHM || "RS256",
+  JWT_AUDIENCE: JWT_AUDIENCE || "graphql.mstacm.org",
+  JWT_ISSUER: JWT_ISSUER || `https://${AUTH0_DOMAIN}/`,
+  JWT_JWKS_URI: JWT_JWKS_URI || `https://${AUTH0_DOMAIN}/.well-known/jwks.json`,
+  JWT_USERINFO_URI: JWT_USERINFO_URI || `https://${AUTH0_DOMAIN}/userinfo`,
   NODE_ENV: process.env.NODE_ENV || "development",
-  SECRET_APP_KEY: process.env.SECRET_APP_KEY || "DEV_KEY",
-  STRIPE_PRIVATE_TOKEN: process.env.STRIPE_PRIVATE_TOKEN || "DEV_KEY",
-  STRIPE_PUBLIC_TOKEN: process.env.STRIPE_PUBLIC_TOKEN || "DEV_KEY",
-  STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET || "DEV_KEY",
+  SECRET_APP_KEY: SECRET_APP_KEY || "DEV_KEY",
+  STRIPE_PRIVATE_TOKEN: STRIPE_PRIVATE_TOKEN || "DEV_KEY",
+  STRIPE_PUBLIC_TOKEN: STRIPE_PUBLIC_TOKEN || "DEV_KEY",
+  STRIPE_WEBHOOK_SECRET: STRIPE_WEBHOOK_SECRET || "DEV_KEY",
   ext: process.env.NODE_ENV !== "production" ? ".ts" : ".js",
   host: process.env.HOST || "0.0.0.0",
   port: parseInt(process.env.NODE_PORT || "3000", 10)
