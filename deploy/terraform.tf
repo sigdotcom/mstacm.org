075787b5a426eae151c6e8eb2cde1a0bc268ea65
Kevin Schoonover <schoonoverkevinm@gmail.com>
2020-03-31 01:26:30 +0000

    Remove deprecated code + add more terraform automations

--- deploy/terraform.tf
+++ deploy/terraform.tf
@@ -16,14 +16,19 @@ variable "spaces_secret_key" {
   description = "DigitalOcean spaces secret key generated at Application & API in the DigitalOcean control panel"
 }
 
+locals {
+  uri = terraform.workspace == "production" ? "mstacm.org" : "devel.mstacm.org"
+}
+
+
 #################
 # Provider
 #################
 provider "digitalocean" {
-  token = "${var.do_token}"
+  token = var.do_token
 
-  spaces_access_id  = "${var.spaces_access_id}"
-  spaces_secret_key = "${var.spaces_secret_key}"
+  spaces_access_id  = var.spaces_access_id
+  spaces_secret_key = var.spaces_secret_key
 }
 
 # See https://www.terraform.io/docs/cloud/migrate/index.html for migration from
@@ -34,22 +39,21 @@ terraform {
     organization = "mstacm"
 
     workspaces {
-      name = "mstacm_org"
+      prefix = "mstacm_org-"
     }
   }
 }
 
 # Add local ssh key for accessing the various resources
 resource "digitalocean_ssh_key" "terraform" {
-  name       = "Terraform Key"
-  public_key = "${file("~/.ssh/mstacm-digitalocean.pub")}"
+  name       = "Terraform Key - ${terraform.workspace}"
+  public_key = file("./.keys/digitalocean-mstacm.pub")
 }
 
-# Add ssh key used in the CircleCI workflow to automatically deploy the
-# application
-resource "digitalocean_ssh_key" "circleci" {
-  name       = "CircleCI Key"
-  public_key = "${file("~/.ssh/mstacm-circleci-deploy.pub")}"
+# Add ssh key used in the to automatically deploy the application
+resource "digitalocean_ssh_key" "deploy" {
+  name       = "Deploy Key - ${terraform.workspace}"
+  public_key = file("./.keys/deploy-master-mstacm.pub")
 }
 
 
@@ -57,54 +61,127 @@ resource "digitalocean_ssh_key" "circleci" {
 # Domains
 #################
 # Domain name for mstacm.org
-# resource "digitalocean_domain" "default" {
-#   name       = "mstacm.org"
-# }
+resource "digitalocean_domain" "default" {
+  name       = local.uri
+}
 
 # Domain name for api.mstacm.org
 resource "digitalocean_domain" "api" {
-  name       = "api.mstacm.org"
-  ip_address = "${digitalocean_droplet.api.ipv4_address}"
+  name       = "api.${local.uri}"
+  ip_address = digitalocean_droplet.api.ipv4_address
 }
 
 # Domain name for resumes.mstacm.org
-# resource "digitalocean_domain" "resumes" {
-#   name       = "resumes.mstacm.org"
-# }
+resource "digitalocean_domain" "resumes" {
+  name       = "resumes.${local.uri}"
+}
 
 # Create a DigitalOcean managed Let's Encrypt Certificate
-# resource "digitalocean_certificate" "resumes" {
-#   name    = "resumes-cert"
-#   type    = "lets_encrypt"
-#   domains = ["${digitalocean_domain.resumes.name}"]
-# }
+resource "digitalocean_certificate" "resumes" {
+  name    = "resumes-cert"
+  type    = "lets_encrypt"
+  domains = [digitalocean_domain.resumes.name]
+}
 
 # Domain name for assets.mstacm.org
-# resource "digitalocean_domain" "assets" {
-#   name       = "assets.mstacm.org"
-# }
+resource "digitalocean_domain" "cdn" {
+  name       = "cdn.${local.uri}"
+}
 
 # Create a DigitalOcean managed Let's Encrypt Certificate
-# resource "digitalocean_certificate" "assets" {
-#   name    = "assets-cert"
-#   type    = "lets_encrypt"
-#   domains = ["${digitalocean_domain.assets.name}"]
-# }
+resource "digitalocean_certificate" "cdn" {
+  name    = "assets-cert"
+  type    = "lets_encrypt"
+  domains = [digitalocean_domain.cdn.name]
+}
+
+#################
+# Domain Records
+#################
+# Additional records off of mstacm.org that point to external services /
+# providers
+
+# Point mstacm.org to netlify
+resource "digitalocean_record" "cname-admin" {
+  domain   = digitalocean_domain.default.name
+  type     = "A"
+  name     = "@"
+  value    = "104.198.14.52"
+}
+
+# Point www.mstacm.org to netlify
+resource "digitalocean_record" "cname-admin" {
+  domain   = digitalocean_domain.default.name
+  type     = "A"
+  name     = "www"
+  value    = "104.198.14.52"
+}
+
+# CNAME to netlify for the resumes page
+resource "digitalocean_record" "cname-resumes" {
+  domain   = digitalocean_domain.default.name
+  type     = "CNAME"
+  name     = "resumes"
+  value    = "mstacm-resumes.netlify.com."
+}
+
+# CNAME to netlify for the profile page
+resource "digitalocean_record" "cname-profile" {
+  domain   = digitalocean_domain.default.name
+  type     = "CNAME"
+  name     = "profile"
+  value    = "profile-mstacm.netlify.com."
+}
+
+# CNAME to netlify for the admin page
+resource "digitalocean_record" "cname-admin" {
+  domain   = digitalocean_domain.default.name
+  type     = "CNAME"
+  name     = "admin"
+  value    = "admin-mstacm.netlify.com."
+}
+
+# CNAME to auth0 for the authentication
+resource "digitalocean_record" "cname-auth0" {
+  domain   = digitalocean_domain.default.name
+  type     = "CNAME"
+  name     = "auth"
+  value    = "mstacm-cd-ko2mjvj6yr6dysrc.edge.tenants.auth0.com."
+}
+
+# CNAME to women.mstacm.org for ACM-W's page
+resource "digitalocean_record" "cname-women" {
+  domain   = digitalocean_domain.default.name
+  type     = "CNAME"
+  name     = "women"
+  value    = "mstacmw.github.io."
+}
+
+# CNAME to comp.mstacm.org for ACM Comp's page
+resource "digitalocean_record" "cname-comp" {
+  domain   = digitalocean_domain.default.name
+  type     = "CNAME"
+  name     = "comp"
+  value    = "sigcomp.github.io."
+}
+
 
 #################
 # Droplets
 #################
 # Create droplet to host the api docker containers
 resource "digitalocean_droplet" "api" {
-  image              = "ubuntu-18-04-x64"
-  name               = "api.mstacm.org"
+  name               = "api.${local.uri}"
+  image              = "docker-18-04"
   region             = "nyc3"
   size               = "s-1vcpu-1gb"
-  ssh_keys           = [
-    "${digitalocean_ssh_key.terraform.fingerprint}",
-    "${digitalocean_ssh_key.circleci.fingerprint}"
-  ]
+  monitoring = true
   private_networking = true
+  tags = ["api"]
+  ssh_keys           = [
+    digitalocean_ssh_key.terraform.fingerprint,
+    digitalocean_ssh_key.deploy.fingerprint
+  ]
 }
 
 #################
@@ -112,7 +189,8 @@ resource "digitalocean_droplet" "api" {
 #################
 # Create a postgres database for all client data
 resource "digitalocean_database_cluster" "default" {
-  name       = "mstacm-postgres-cluster"
+  name       = terraform.workspace == "production" ? "mstacm-postgres-cluster" : "develop-mstacm-postgres-cluster"
+
   engine     = "pg"
   version    = "11"
   size       = "db-s-1vcpu-1gb"
@@ -120,15 +198,26 @@ resource "digitalocean_database_cluster" "default" {
   node_count = 1
 }
 
+resource "digitalocean_database_db" "default" {
+  cluster_id = digitalocean_database_cluster.default.id
+  name       = "phoenix"
+}
+
+resource "digitalocean_database_user" "default" {
+  cluster_id = digitalocean_database_cluster.default.id
+  name       = "phoenix"
+}
+
+
 #################
 # Firewalls
 #################
 # create a firewall that only accepts port 22/80/443 traffic
 resource "digitalocean_firewall" "mstacm-firewall" {
-  name = "mstacm-firewall"
+  name = "mstacm-firewall-${terraform.workspace}"
 
   droplet_ids = [
-    "${digitalocean_droplet.api.id}"
+    digitalocean_droplet.api.id
   ]
 
   inbound_rule {
@@ -167,72 +256,67 @@ resource "digitalocean_firewall" "mstacm-firewall" {
 #################
 # Space for storing all CDN files
 resource "digitalocean_spaces_bucket" "cdn" {
-  name   = "mstacm-cdn"
+  name   = terraform.workspace == "production" ? "mstacm-cdn" : "mstacm-cdn-develop"
   region = "nyc3"
 }
 
 # Add a CDN endpoint with a custom sub-domain to the assets bucket
-# resource "digitalocean_cdn" "cdn" {
-#   origin         = "${digitalocean_spaces_bucket.cdn.bucket_domain_name}"
-#   custom_domain  = "${digitalocean_domain.cdn.name}"
-#   certificate_id = "${digitalocean_certificate.cdn.id}"
-# }
-
-# Add a CDN endpoint for the CDN bucket
-resource "digitalocean_cdn" "primary" {
-  origin = "${digitalocean_spaces_bucket.cdn.bucket_domain_name}"
+resource "digitalocean_cdn" "cdn" {
+  origin         = digitalocean_spaces_bucket.cdn.bucket_domain_name
+  custom_domain  = digitalocean_domain.cdn.name
+  certificate_id = digitalocean_certificate.cdn.id
 }
- 
+
+resource "digitalocean_spaces_bucket" "resumes" {
+  name   = terraform.workspace == "production" ? "mstacm-resumes" : "mstacm-resumes-develop"
+  region = "nyc3"
+}
+
+# Add a CDN endpoint with a custom sub-domain to the assets bucket
+resource "digitalocean_cdn" "resumes" {
+  origin         = digitalocean_spaces_bucket.resumes.bucket_domain_name
+  custom_domain  = digitalocean_domain.resumes.name
+  certificate_id = digitalocean_certificate.resumes.id
+}
+
 #################
 # Projects
 #################
 # Project for storing all of the resources for mstacm.org
 resource "digitalocean_project" "default" {
-  name        = "mstacm.org"
-  description = "All resources for *.mstacm.org."
+  name        = local.uri
+  description = "All resources for *.${local.uri}."
   purpose     = "Web Application"
   environment = "Production"
   resources   = [
-    "${digitalocean_droplet.api.urn}",
-    "${digitalocean_spaces_bucket.cdn.urn}",
-    "${digitalocean_database_cluster.default.urn}"
+    digitalocean_droplet.api.urn,
+    digitalocean_spaces_bucket.cdn.urn,
+    digitalocean_database_cluster.default.urn
   ]
 }
 
-#################
-# Provisioners
-#################
-# create an ansible inventory file
-resource "null_resource" "ansible-provision" {
-  depends_on = ["digitalocean_droplet.api"]
-
-  provisioner "local-exec" {
-    command = "echo '${digitalocean_droplet.api.name} ansible_host=${digitalocean_droplet.api.ipv4_address}' > production"
-  }
-}
-
 output "postgres_private_host" {
-    value = "${digitalocean_database_cluster.default.private_host}"
+    value = digitalocean_database_cluster.default.private_host
     description = "The database's hostname accessible from resources in same region"
 }
 
 output "postgres_database" {
-    value = "${digitalocean_database_cluster.default.database}"
+    value = digitalocean_database_cluster.default.database
     description = "The database's default database"
 }
 
 output "postgres_port" {
-    value = "${digitalocean_database_cluster.default.port}"
+    value = digitalocean_database_cluster.default.port
     description = "The port used to access the database"
 }
 
 output "postgres_user" {
-    value = "${digitalocean_database_cluster.default.user}"
+    value = digitalocean_database_cluster.default.user
     description = "The database's default user"
 }
 
 output "postgres_password" {
-    value = "${digitalocean_database_cluster.default.password}"
+    value = digitalocean_database_cluster.default.password
     description = "The database's default user password"
     sensitive = true
 }
