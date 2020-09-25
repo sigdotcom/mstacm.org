161bc0bcd1dde61c8614afce6d4dec937442844c
Deniz <denizkerim@yahoo.com>
2020-09-24 21:42:20 -0500

    feature/profile-page minor changes, setup styles for events component

--- apps/profile-web/src/components/ProfilePage/events.tsx
+++ apps/profile-web/src/components/ProfilePage/events.tsx
@@ -1,14 +1,45 @@
 import React from "react";
 
-// import styled, { AnyStyledComponent } from "styled-components";
+import styled, { AnyStyledComponent } from "styled-components";
 
-// const Temp: AnyStyledComponent = styled.div`
-// `;
+const EventsWrapper: AnyStyledComponent = styled.div`
+  position: absolute;
+  bottom: 0;
+  transform: translateY(50%);
+  background: white;
+  border-radius: 50px;
+  box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.15);
+`;
+
+const WrapperWrapper: AnyStyledComponent = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 4rem;
+  margin: 0 1.5rem;
+`;
+
+const NumberAttended: AnyStyledComponent = styled.div`
+  font-size: 3rem;
+  font-weight: 700;
+  color: #333333;
+  margin-right: .5rem;
+`;
+
+const EventsAttended: AnyStyledComponent = styled.div`
+  font-size: 0.94rem;
+  font-weight: 400;
+  width: 4rem;
+  color: #333333;
+`;
 
 export const Events: React.FC<{}> = () => {
   return (
-    <div>
-      <h1>Events</h1>
-    </div>
+    <EventsWrapper>
+      <WrapperWrapper>
+        <NumberAttended>35</NumberAttended>
+        <EventsAttended>Events attended</EventsAttended>
+      </WrapperWrapper>
+    </EventsWrapper>
   );
 };
--- apps/profile-web/src/components/ProfilePage/index.tsx
+++ apps/profile-web/src/components/ProfilePage/index.tsx
@@ -1,31 +1,25 @@
 import React from "react";
 
-import { Events } from "./events";
+import { ProfileHeader } from "./profileHeader";
 import { Membership } from "./membership";
 import { Participation } from "./participation";
 import { QuickAccess } from "../QuickAccess";
 
 import styled, { AnyStyledComponent } from "styled-components";
 
-import { useAuth0 } from "../../utils/react-auth0-wrapper";
-
 const ProfileWrapper: AnyStyledComponent = styled.div`
   background: white;
-`;
-
-const ProfileHeader: AnyStyledComponent = styled.div`
-  background: #F4F5F8;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100%;
+  font-family: "Nunito Sans";
 `;
 
 export const ProfilePage: React.FC<{}> = () => {
-  const { user } = useAuth0();
-
   return (
     <ProfileWrapper>
-      <ProfileHeader>
-        <h1>Hello {user.name}!</h1>
-      </ProfileHeader>
-      <Events />
+      <ProfileHeader />
       <Membership />
       <Participation />
       <QuickAccess />
