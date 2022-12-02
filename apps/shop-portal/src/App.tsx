06be0231700c37b4a1e1f3253ced2965e26bb025
Parsa Hajiha <hajihaparsa2004@gmail.com>
2022-12-01 18:11:05 -0600

    background added

--- apps/shop-portal/src/App.tsx
+++ apps/shop-portal/src/App.tsx
@@ -1,11 +1,36 @@
 import React from "react";
 import "./index.css";
 import NavBar from "./components/NavBar/NavBar";
+import styled from "styled-components";
 
 function App() {
+    const HeroImage = styled.img`
+    display: none;
+  
+    @media screen and (min-width: 768px) {
+      width: 800px;
+      display: block;
+      opacity: 0.5;
+      position: absolute;
+      right: 300px;
+      bottom: -250px;
+      pointer-events: none;
+    }
+  
+    @media screen and (min-width: 1300px) and (min-height: 800px) {
+      width: 940px;
+    }
+  
+    @media screen and (min-height: 1000px) {
+      width: 800px;
+      bottom: -250px;
+    }
+  `;
     return (
         <div className="App">
             <NavBar />
+            {/* <img src="https://cdn.mstacm.org/static/acm-logo-large.svg"></img> */}
+            <HeroImage src="https://cdn.mstacm.org/static/acm-logo-large.svg" />
         </div>
     );
 }
---
+++ apps/shop-portal/src/config.ts
@@ -0,0 +1,31 @@
+export interface IConfig {
+    ACCESS_TOKEN_KEY: string;
+    API_URI: string;
+    AUTH0_AUDIENCE: string;
+    AUTH0_CLIENT_ID: string;
+    AUTH0_DOMAIN: string;
+    CDN_URI: string;
+    REDEMPTION_CODE_KEY: string;
+    REDEMPTION_QUERY_PARAM_KEY: string;
+    STRIPE_PUBLIC_KEY: string;
+  }
+  
+  const IS_PROD: boolean = process.env.NODE_ENV === "production";
+  
+  export const config: IConfig = {
+    ACCESS_TOKEN_KEY: "auth0_access_token",
+    API_URI: IS_PROD
+      ? "https://api.mstacm.org/graphql"
+      : "http://localhost/graphql",
+    AUTH0_AUDIENCE: process.env.REACT_APP_AUTH0_AUDIENCE || "graphql.mstacm.org",
+    AUTH0_CLIENT_ID:
+      process.env.REACT_APP_AUTH0_CLIENT_ID || "na6x6uijGdUE2zpiu9MqmSRQwGUdhNQl",
+    AUTH0_DOMAIN: process.env.REACT_APP_AUTH0_DOMAIN || "mstacm-test.auth0.com",
+    CDN_URI: process.env.REACT_APP_CDN_URI || "https://cdn.mstacm.org",
+    REDEMPTION_CODE_KEY: "redemption_code",
+    REDEMPTION_QUERY_PARAM_KEY: "redeem",
+    STRIPE_PUBLIC_KEY:
+      process.env.REACT_APP_STRIPE_PUBLIC_KEY ||
+      "pk_test_5K6vSoTGrPyY2cBw3nNSixV300Z8JOVkl7"
+  };
+  
